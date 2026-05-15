25586e46234501b8af1d71df96d0bb4a21cc5c8b
aryanpatel287 <aryanpatel.me@gmail.com>
2026-05-15 17:56:15 +0530

    Redis setup in middleware

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import userModel from '../models/user.model.js';
 import envConfig from '../config/envconfig.js';
+import redis from '../config/cache.js';
 
 async function authUser(req, res, next) {
     const token = req.cookies.token;
@@ -10,6 +11,15 @@ async function authUser(req, res, next) {
             message: 'Invalid token',
             success: false,
             error: 'token not found',
+        });
+    }
+
+    const isTokenBlacklisted = await redis.get(token);
+    if (isTokenBlacklisted) {
+        return res.status(401).json({
+            message: 'Invalid token',
+            success: false,
+            error: 'Token is blacklisted',
         });
     }
 
